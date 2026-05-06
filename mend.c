687a4d4fda73ae9cfcadc7bd797938fc968b4108
Harol Rosero <harol.rosero@udla.edu.ec>
2026-05-06 15:17:50 -0500

    en este commit se agrego la variable entero para el puntero

---
+++ mend.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+   
+   int num = 150, a;
+   int *x;        
+   x = &num;        
+   a = *x;          
+ 
+   printf("%d %d %p\n", num, a, x);
+
+   float pi = 3.1415, decimal;
+   float *ptrF;     
+   ptrF = &pi;      
+   decimal = *ptrF; 
+   printf("%.3f %.3f %p\n", pi, decimal, ptrF);
+
+   char latra
+
+
+   return 0;
+}
